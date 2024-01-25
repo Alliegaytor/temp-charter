2685f90a80cdbdc1d61edb6ac87fb9c3a8892681
Allison Smith <allisondaniellesmith@protonmail.com>
2024-01-25 19:10:24 +1100

    graph past 7d & stats on min / max recorded

--- graphData.py
+++ graphData.py
@@ -18,7 +18,6 @@ mpl.rcParams['xtick.direction'] = 'in'
 mpl.rcParams['figure.dpi'] = 200
 plt.rcParams['axes.xmargin'] = 0
 
-
 data = pd.read_csv('merged.csv', dayfirst=True, usecols=['time', 'temp_x', 'temp_y'], parse_dates=["time"], index_col=["time"])
 data.columns = ['T_indoors', 'T_outdoors']
 
@@ -36,7 +35,18 @@ data['delta'].plot().set_ylabel('ΔT (T_indoors - T_outdoors)')
 plt.savefig('plot_delta.png')
 plt.show()
 
+# last 7 days @ 30 min interval = 336 data points
+# TODO: Figure out a better way of using last x days
+interval = data.index[-1] - data.index[-2]
+interval7d = round('7D' / interval) # python type magic
+
+data.tail(interval7d).plot().set_ylabel('temperature Celcius')
+plt.savefig('plot_7day.png')
+plt.show()
+
 # Min, max, mean per 24hr
+
+dataold = data.copy()
 
 del data['delta']
 del data['T_outdoors']
@@ -60,3 +70,7 @@ plt.show()
 print('\n' + '=' * 20)
 print('Days above 30°C:', sum(data['T_max'] >= 30))
 print('Days below 20°C:', sum(data['T_min'] <=20))
+
+print('\n' + '=' * 20)
+print('Maximum Temp:', max(data['T_max']), '°C', 'at', dataold['T_indoors'].idxmax())
+print('Minimum Temp:', min(data['T_min']), '°C', 'at', dataold['T_indoors'].idxmin())
